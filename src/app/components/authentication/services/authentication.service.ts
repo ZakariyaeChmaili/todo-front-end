07987cf564d583b4361bfdbac0b0bfc6f76c8a74
ZakariyaeChmaili <zakaria.chmaili99@gmail.com>
2023-09-24 15:00:12 +0100

    nothing

--- src/app/components/authentication/services/authentication.service.ts
+++ src/app/components/authentication/services/authentication.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {env} from 'env';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
+  private api : string = env.api;
   constructor(private http:HttpClient) { }
 
 
   login(user:any){
-    return this.http.post('http://localhost:8000/api/login',user);
+    return this.http.post(`${this.api}/login`,user);
   }
 
   create(user:any){
-    return this.http.post('http://localhost:8000/api/register',user);
+    return this.http.post(`${this.api}/register`,user);
   }
 }
--- src/app/components/layout/services/todo.service.ts
+++ src/app/components/layout/services/todo.service.ts
@@ -1,38 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {env} from 'env';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  private api : string = env.api;
   constructor(private http : HttpClient) { }
 
   getTodos(){
-    return this.http.get('http://localhost:8000/api/todos');
+    return this.http.get(`${this.api}/api/todos`);
   }
 
   addTask(task : any){
-    return this.http.post('http://localhost:8000/api/todos', task);
+    return this.http.post(`${this.api}/api/todos`, task);
   }
 
 
   markAsDone(task : any){
-    return this.http.put('http://localhost:8000/api/todos/'+task.id, task);
+    return this.http.put(`${this.api}/api/todos/`+task.id, task);
   }
 
   deleteTodo(task:any){
     console.log(task.node._id)
-    return this.http.delete('http://localhost:8000/api/todos/'+task.node._id);
+    return this.http.delete(`${this.api}/api/todos/`+task.node._id);
   }
 
 
   deleteManyTodo(ids:any[]){
-    return this.http.post('http://localhost:8000/api/todos/deleteMany', ids);
+    return this.http.post(`${this.api}/api/todos/deleteMany`, ids);
   }
 
   updateTask(task:any,id:any){
-    return this.http.put('http://localhost:8000/api/todos/'+id, task);
+    return this.http.put(`${this.api}/api/todos/`+id, task);
 
   }
 
